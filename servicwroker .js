10a5fa69969a4de113ab88690af6a32f276f3868
naikrohan144873-ron <naikrohan144873@gmail.com>
2025-11-07 20:21:57 +0530

    Update servicwroker .js

--- servicwroker .js
+++ servicwroker .js
@@ -11,21 +11,47 @@ const STATIC_ASSETS = [
   '/manifest.json',
   '/icon-512.svg'
 ];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS)));
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
   self.skipWaiting();
 });
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      return caches.open(CACHE_NAME).then(c => { c.put(e.request, res.clone()); return res; });
-    })).catch(()=>caches.match('/index.html'))
-  );
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(self.clients.claim());
 });
-self.addEventListener('message', e => {
-  if (e.data && e.data.type === 'SHOW_NOTIFICATION') {
-    const {title, options} = e.data.payload;
+
+self.addEventListener('fetch', event => {
+  try {
+    if (event.request.mode === 'navigate') {
+      event.respondWith(
+        fetch(event.request).then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        }).catch(() => caches.match('/index.html'))
+      );
+      return;
+    }
+
+    event.respondWith(
+      caches.match(event.request).then(cached => {
+        return cached || fetch(event.request).then(res => {
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, res.clone()));
+          return res;
+        });
+      }).catch(()=>caches.match('/index.html'))
+    );
+  } catch (e) {
+    event.respondWith(fetch(event.request));
+  }
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+    const {title, options} = event.data.payload;
     self.registration.showNotification(title, options);
   }
 });
